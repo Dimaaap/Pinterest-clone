73a013ef77d85a76fd84289401dd897cfcf274a8
Dimaaap <procdima49@gmail.com>
2023-11-15 21:14:22 +0300

    Add a second modal and fixed some bugs

--- pinterest/business/static/business/js/circle_indicator.js
+++ pinterest/business/static/business/js/circle_indicator.js
@@ -1,27 +1,33 @@
-let currentStep = parseInt(localStorage.getItem('currentStep')) || 0;
+let currentStep = parseInt(localStorage.getItem('currentStep')) || 1;
 const submitButton = document.getElementById("submit-button");
 const backButton = document.getElementById("back-button") || null;
-const firstForm = document.getElementById("first-form");
-const secondForm = document.getElementById("second-form");
-const thirdForm = document.getElementById("third-form");
-const fourthForm = document.getElementById("fourth-form");
+const forms = document.querySelectorAll(".form");
 
 
-const updateSetIndicator = () => {
-    const stepIndicators = document.querySelectorAll(".step-circle");
-    stepIndicators.forEach((indicator, index) => {
-        indicator.classList.toggle("active", index === currentStep);
-        indicator.classList.toggle('completed', index < currentStep);
-    })
-}
+let formsIndexMap = new Map([
+    ["first-form", 1],
+    ["second-form", 2],
+    ["third-form", 3],
+    ["fourth-form", 4]
+])
 
 const goToStep = (step) => {
     currentStep = step;
     localStorage.setItem('currentStep', currentStep);
-    updateSetIndicator();
+    updateSetIndicator(step);
 }
 
-let formsArray = [firstForm, secondForm, thirdForm, fourthForm];
+document.addEventListener('DOMContentLoaded', (event) => {
+    activeForm = forms[0].id;
+    currentIndex = formsIndexMap.get(activeForm) - 1;
+    goToStep(currentIndex)
+})
 
-
-updateSetIndicator();
+const updateSetIndicator = (index) => {
+    const stepIndicators = document.querySelectorAll(".step-circle");
+    stepIndicators.forEach((indicator, indIndex) => {
+        if(indIndex <= index){
+            indicator.classList.add("active");
+        }
+    })
+}
